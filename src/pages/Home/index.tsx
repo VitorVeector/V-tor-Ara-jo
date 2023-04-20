f178f2820301fa6aef5f52f6842646947cb45f2e
Vitor Araujo <job_vitoraraujo@outlook.com>
2023-04-20 11:59:02 -0300

    :lipstick: Style: Blob animation home picture

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,23 +1,26 @@
-import './style.scss';
-import profile from '../../assets/Profile.jpeg';
-import { motion } from 'framer-motion';
+import "./style.scss";
+import profile from "../../assets/Profile.jpeg";
+import { motion } from "framer-motion";
 
 const skills = [
-  { name: 'JavaScript', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
-  { name: 'TypeScript', link: 'https://www.typescriptlang.org/' },
-  { name: 'React.JS', link: 'https://reactjs.org/' },
-  { name: 'React Native', link: 'https://reactnative.dev/' },
-  { name: 'Next.JS', link: 'https://nextjs.org/' },
-  { name: 'Node.JS', link: 'https://nodejs.org/en/' },
-  { name: 'Express.JS', link: 'https://expressjs.com/' },
-  { name: 'S.O.L.I.D', link: 'https://en.wikipedia.org/wiki/SOLID' },
-  { name: 'Sass / Css', link: 'https://sass-lang.com/' },
-  { name: 'ChakraUI', link: 'https://chakra-ui.com/' },
-  { name: 'StyledComponents', link: 'https://styled-components.com/' },
-  { name: 'MaterialUI', link: 'https://mui.com/' },
-  { name: 'PrismicCMS', link: 'https://prismic.io/' },
-  { name: 'Stripe', link: 'https://stripe.com/' },
-  { name: 'Framer Motion', link: 'https://www.framer.com/motion/' },
+    {
+        name: "JavaScript",
+        link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    },
+    { name: "TypeScript", link: "https://www.typescriptlang.org/" },
+    { name: "React.JS", link: "https://reactjs.org/" },
+    { name: "React Native", link: "https://reactnative.dev/" },
+    { name: "Next.JS", link: "https://nextjs.org/" },
+    { name: "Node.JS", link: "https://nodejs.org/en/" },
+    { name: "Express.JS", link: "https://expressjs.com/" },
+    { name: "S.O.L.I.D", link: "https://en.wikipedia.org/wiki/SOLID" },
+    { name: "Sass / Css", link: "https://sass-lang.com/" },
+    { name: "ChakraUI", link: "https://chakra-ui.com/" },
+    { name: "StyledComponents", link: "https://styled-components.com/" },
+    { name: "MaterialUI", link: "https://mui.com/" },
+    { name: "PrismicCMS", link: "https://prismic.io/" },
+    { name: "Stripe", link: "https://stripe.com/" },
+    { name: "Framer Motion", link: "https://www.framer.com/motion/" },
 ];
 
 const skillVariants = skills.map((skill, index) => ({
@@ -26,46 +29,60 @@ const skillVariants = skills.map((skill, index) => ({
 }));
 
 export const Home = () => {
-  return (
-    <header>
-
-      <div className="home-container">
-        <div className="home-content">
-          <div className="home-info">
-            <p>
-              Hi, my name is Vitor and I am a Junior Full-Stack Web/Mobile Developer based in São Paulo, Brazil. I have strong skills in JavaScript and I am currently studying Systems Analysis and Development at Estácio University. I am passionate about technology and love working collaboratively with others to contribute to the success of projects.
-            </p>
-            <div className='home-info_skills'>
-              <h3>my knowledge includes:</h3>
-              <div className='info_skills-list'>
-                <ul>
-                  {skills.map((skill, index) => (
-                    <motion.div 
-                      key={index}
-                      variants={skillVariants[index]}
-                      initial="hidden"
-                      animate="visible">
-                      <li className='info_skills-item'>
-                        <span><a href={skill.link} target='_blank' rel='noreferrer'>{skill.name}</a></span>
-                      </li>
-                    </motion.div>
-                  ))}
-                </ul>
-                <img src={profile} alt="" />
-              </div>
+    return (
+        <header>
+            <div className="home-container">
+                <div className="home-content">
+                    <div className="home-info">
+                        <p>
+                            Hi, my name is Vitor and I am a Junior Full-Stack
+                            Web/Mobile Developer based in São Paulo, Brazil. I
+                            have strong skills in JavaScript and I am currently
+                            studying Systems Analysis and Development at Estácio
+                            University. I am passionate about technology and
+                            love working collaboratively with others to
+                            contribute to the success of projects.
+                        </p>
+                        <div className="home-info_skills">
+                            <h3>my knowledge includes:</h3>
+                            <div className="info_skills-list">
+                                <ul>
+                                    {skills.map((skill, index) => (
+                                        <motion.div
+                                            key={index}
+                                            variants={skillVariants[index]}
+                                            initial="hidden"
+                                            animate="visible"
+                                        >
+                                            <li className="info_skills-item">
+                                                <span>
+                                                    <a
+                                                        href={skill.link}
+                                                        target="_blank"
+                                                        rel="noreferrer"
+                                                    >
+                                                        {skill.name}
+                                                    </a>
+                                                </span>
+                                            </li>
+                                        </motion.div>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="home-imgs">
+                        <motion.div
+                            initial={{ opacity: 0, y: 60 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 200 }}
+                            transition={{ duration: 1, delay: 0.1 }}
+                        >
+                            <img src={profile} alt="" />
+                        </motion.div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="home-imgs">
-            <motion.div
-              initial={{opacity: 0, y: 60}}
-              animate={{opacity: 1, y: 0}}
-              exit={{opacity: 0, y: 200}}
-              transition={{ duration: 1, delay: .1 }}>
-              <img src={profile} alt="" />
-            </motion.div>
-          </div>
-        </div>
-      </div>
-    </header>
-  )
+        </header>
+    );
 };
