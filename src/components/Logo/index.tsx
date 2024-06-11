f873839a5b05a2e8cb78789cc9cd08ee04d11708
Vítor Araujo <job_vitoraraujo@outlook.com>
2024-06-11 10:42:55 -0300

    :mag: SEO: Add h1

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -3,7 +3,7 @@ import './style.scss'
 export const Logo = () => {
     return (
         <div className='header-logo'>
-            <h3>Vítor Silva</h3>
+            <h1>Vítor Silva</h1>
         </div>
     )
 }
